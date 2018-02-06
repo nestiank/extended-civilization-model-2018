7f0ccd03c56166d43d0802d7c63aa59895416679
16박진석 <key13689@gmail.com>
2018-02-07 00:40:41 +0900

    Working GenghisKhan's skill

--- CivModel.Finno/GenghisKhan.cs
+++ CivModel.Finno/GenghisKhan.cs
@@ -24,10 +24,60 @@ namespace CivModel.Common
         private readonly IActorAction _movingAttackAct;
         public override IActorAction MovingAttackAct => _movingAttackAct;
 
+        public override IReadOnlyList<IActorAction> SpecialActs => _specialActs;
+        private readonly IActorAction[] _specialActs = new IActorAction[1];
+
         public GenghisKhan(Player owner, Terrain.Point point) : base(owner, point)
         {
             _holdingAttackAct = new AttackActorAction(this, false);
             _movingAttackAct = new AttackActorAction(this, true);
+            _specialActs[0] = new GenghisKhanAction(this);
+        }
+
+        private class GenghisKhanAction : IActorAction
+        {
+            private readonly GenghisKhan _owner;
+            public Actor Owner => _owner;
+
+            public bool IsParametered => false;
+
+            public GenghisKhanAction(GenghisKhan owner)
+            {
+                _owner = owner;
+            }
+
+            public int LastSkillCalled = -2;
+
+            public int GetRequiredAP(Terrain.Point? pt)
+            {
+                if (pt != null)
+                    return -1;
+                if (!_owner.PlacedPoint.HasValue)
+                    return -1;
+                if (Owner.Owner.Game.TurnNumber < LastSkillCalled + 2)
+                    return -1;
+
+                return 1;
+            }
+
+            public void Act(Terrain.Point? pt)
+            {
+                if (pt != null)
+                    throw new ArgumentException("pt is invalid");
+                if (!_owner.PlacedPoint.HasValue)
+                    throw new InvalidOperationException("Actor is not placed yet");
+                if (Owner.Owner.Game.TurnNumber < LastSkillCalled + 5)
+                    throw new InvalidOperationException("Skill is not turned on");
+                if (Owner.PlacedPoint.Value.TileOwner == Owner.Owner)
+                    throw new InvalidOperationException("You have the Territory");
+
+                Owner.Owner.AddTerritory(Owner.PlacedPoint.Value);
+                if (Owner.PlacedPoint.Value.TileBuilding != null)
+                {
+                    Owner.PlacedPoint.Value.TileBuilding.Destroy();
+                }
+
+            }
         }
     }
 
