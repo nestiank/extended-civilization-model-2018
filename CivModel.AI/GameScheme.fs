98f8a39fc3575e0b359854521cb9f9aae412e89f
dlarudgus20 <dlarudgus20@naver.com>
2018-02-22 01:04:44 +0900

    pretty property

--- CivModel.AI/GameScheme.fs
+++ CivModel.AI/GameScheme.fs
@@ -6,11 +6,11 @@ open CivModel
 
 type public GameSchemeFactory() =
     interface IGameSchemeFactory with
-        member val Guid = Guid("1A698AD2-DA40-455C-BD60-1A40C14FB76D") with get
-        member val SchemeType = typeof<GameScheme> with get
+        member val Guid = Guid("1A698AD2-DA40-455C-BD60-1A40C14FB76D")
+        member val SchemeType = typeof<GameScheme>
 
-        member val Dependencies = [ ] :> IEnumerable<Guid> with get
-        member val KnownSchemeFactories = [ ] :> IEnumerable<IGameSchemeFactory> with get
+        member val Dependencies = [ ] :> IEnumerable<Guid>
+        member val KnownSchemeFactories = [ ] :> IEnumerable<IGameSchemeFactory>
 
         member this.Create() = GameScheme(this) :> IGameScheme
 
