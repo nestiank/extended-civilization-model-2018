56d28e96827d8d7cd761f86f422f0ca355d8ebcf
16박진석 <key13689@gmail.com>
2018-02-25 13:33:40 +0900

    도시 액티브 구현

--- CivModel.Hwan/HwanEmpireCity.cs
+++ CivModel.Hwan/HwanEmpireCity.cs
@@ -13,8 +13,86 @@ namespace CivModel.Hwan
 
         public override double MaxHP => 500;
 
+        public override void PostTurn()
+        {
+            this.RemainHP = Math.Min(500, (this.RemainHP + 20));
+        }
+
+        public override IReadOnlyList<IActorAction> SpecialActs => _specialActs;
+        private readonly IActorAction[] _specialActs = new IActorAction[1];
+
         public HwanEmpireCity(Player player, Terrain.Point point) : base(player, point)
         {
+            _specialActs[0] = new HwanEmpireCityAction(this);
+        }
+
+        private class HwanEmpireCityAction : IActorAction
+        {
+            private readonly HwanEmpireCity _owner;
+            public Actor Owner => _owner;
+
+            public bool IsParametered => false;
+
+            public HwanEmpireCityAction(HwanEmpireCity owner)
+            {
+                _owner = owner;
+            }
+
+            public int GetRequiredAP(Terrain.Point? pt)
+            {
+                if (pt != null)
+                    return -1;
+                if (!_owner.PlacedPoint.HasValue)
+                    return -1;
+
+                return 0;
+            }
+
+            public void Act(Terrain.Point? pt)
+            {
+                if (pt != null)
+                    throw new ArgumentException("pt is invalid");
+                if (!_owner.PlacedPoint.HasValue)
+                    throw new InvalidOperationException("Actor is not placed yet");
+
+                int A = Owner.PlacedPoint.Value.Position.A;
+                int B = Owner.PlacedPoint.Value.Position.B;
+                int C = Owner.PlacedPoint.Value.Position.C;
+
+                RealAction(A + 1, B - 1, C);
+                RealAction(A + 1, B, C - 1);
+                RealAction(A, B + 1, C - 1);
+                RealAction(A - 1, B + 1, C);
+                RealAction(A - 1, B, C + 1);
+                RealAction(A, B - 1, C + 1);
+
+                RealAction(A + 2, B - 2, C);
+                RealAction(A + 2, B - 1, C - 1);
+                RealAction(A + 2, B, C - 2);
+                RealAction(A + 1, B + 1, C - 2);
+                RealAction(A, B + 2, C - 2);
+                RealAction(A - 1, B + 2, C - 1);
+                RealAction(A - 2, B + 2, C);
+                RealAction(A - 2, B + 1, C + 1);
+                RealAction(A - 2, B, C + 2);
+                RealAction(A - 1, B - 1, C + 2);
+                RealAction(A, B - 2, C + 2);
+                RealAction(A + 1, B - 2, C + 1);
+            }
+
+            public void RealAction(int A,int B,int C)
+            {
+                if (0 <= B + (C + Math.Sign(C)) / 2 && B + (C + Math.Sign(C)) / 2 < Owner.PlacedPoint.Value.Terrain.Width && 0 <= C && C < Owner.PlacedPoint.Value.Terrain.Height)
+                {
+                    if ((Owner.PlacedPoint.Value.Terrain.GetPoint(A, B, C)).Unit != null)
+                    {
+                        if ((Owner.PlacedPoint.Value.Terrain.GetPoint(A, B, C)).Unit.Owner != Owner.Owner)
+                        {
+                            Owner.AttackTo(30, (Owner.PlacedPoint.Value.Terrain.GetPoint(A, B, C)).Unit, 0, false, true);
+                        }
+                    }
+                }
+            }
         }
 
         protected override void OnProcessCreation()
@@ -26,11 +104,7 @@ namespace CivModel.Hwan
                 if (pt.HasValue)
                     Owner.TryAddTerritory(pt.Value);
             }
-        }
-        public override void PostTurn()
-        {
-            this.RemainHP = Math.Min(500, (this.RemainHP + 20));
-        }
+        }     
     }
 
     public class HwanEmpireCityProductionFactory : ITileObjectProductionFactory
