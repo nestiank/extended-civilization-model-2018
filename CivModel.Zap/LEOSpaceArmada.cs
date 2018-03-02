363e5db5ae89f14b7e4bfe5feac02c50769ed408
16박진석 <key13689@gmail.com>
2018-03-02 22:26:19 +0900

    Zap국

---
+++ CivModel.Zap/LEOSpaceArmada.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CivModel.Zap
+{
+    public class LEOSpaceArmada : Unit
+    {
+        public static Guid ClassGuid { get; } = new Guid("8CA7B018-F783-4A65-9AB2-FC37A77DE6C0");
+        public override Guid Guid => ClassGuid;
+
+        public static readonly ActorConstants Constants = new ActorConstants
+        {
+            MaxAP = 2,
+            MaxHP = 25,
+            AttackPower = 8,
+            DefencePower = 3,
+            GoldLogistics = 20,
+            FullLaborForRepair = 2,
+            BattleClassLevel = 2
+        };
+
+        private readonly IActorAction _holdingAttackAct;
+        public override IActorAction HoldingAttackAct => _holdingAttackAct;
+
+        private readonly IActorAction _movingAttackAct;
+        public override IActorAction MovingAttackAct => _movingAttackAct;
+
+        public LEOSpaceArmada(Player owner, Terrain.Point point) : base(owner, Constants, point)
+        {
+            _holdingAttackAct = new AttackActorAction(this, false);
+            _movingAttackAct = new AttackActorAction(this, true);
+        }
+    }
+
+    public class LEOSpaceArmadaProductionFactory : ITileObjectProductionFactory
+    {
+        private static Lazy<LEOSpaceArmadaProductionFactory> _instance
+            = new Lazy<LEOSpaceArmadaProductionFactory>(() => new LEOSpaceArmadaProductionFactory());
+        public static LEOSpaceArmadaProductionFactory Instance => _instance.Value;
+        private LEOSpaceArmadaProductionFactory()
+        {
+        }
+
+        public ActorConstants ActorConstants => LEOSpaceArmada.Constants;
+
+        public double TotalLaborCost => 35;
+        public double LaborCapacityPerTurn => 15;
+        public double TotalGoldCost => 50;
+        public double GoldCapacityPerTurn => 20;
+
+        public Production Create(Player owner)
+        {
+            return new TileObjectProduction(this, owner);
+        }
+        public bool IsPlacable(TileObjectProduction production, Terrain.Point point)
+        {
+            return point.Unit == null
+                && point.TileBuilding is CivModel.Common.CityCenter
+                && point.TileBuilding.Owner == production.Owner;
+        }
+        public TileObject CreateTileObject(Player owner, Terrain.Point point)
+        {
+            return new LEOSpaceArmada(owner,point);
+        }
+    }
+}
---
+++ CivModel.Zap/ZapNinja.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CivModel.Zap
+{
+    public class ZapNinja : Unit
+    {
+        public static Guid ClassGuid { get; } = new Guid("04DBFD22-3990-48AC-BAF1-095BDAEFCB57");
+        public override Guid Guid => ClassGuid;
+
+        public static readonly ActorConstants Constants = new ActorConstants
+        {
+            MaxAP = 2,
+            MaxHP = 18,
+            AttackPower = 10,
+            DefencePower = 3,
+            GoldLogistics = 30,
+            FullLaborForRepair = 2,
+            BattleClassLevel = 3
+        };
+
+        private readonly IActorAction _holdingAttackAct;
+        public override IActorAction HoldingAttackAct => _holdingAttackAct;
+
+        private readonly IActorAction _movingAttackAct;
+        public override IActorAction MovingAttackAct => _movingAttackAct;
+
+        public ZapNinja(Player owner, Terrain.Point point) : base(owner, Constants, point)
+        {
+            _holdingAttackAct = new AttackActorAction(this, false);
+            _movingAttackAct = new AttackActorAction(this, true);
+        }
+    }
+
+    public class ZapNinjaProductionFactory : ITileObjectProductionFactory
+    {
+        private static Lazy<ZapNinjaProductionFactory> _instance
+            = new Lazy<ZapNinjaProductionFactory>(() => new ZapNinjaProductionFactory());
+        public static ZapNinjaProductionFactory Instance => _instance.Value;
+        private ZapNinjaProductionFactory()
+        {
+        }
+
+        public ActorConstants ActorConstants => DecentralizedMilitary.Constants;
+
+        public double TotalLaborCost => 50;
+        public double LaborCapacityPerTurn => 20;
+        public double TotalGoldCost => 75;
+        public double GoldCapacityPerTurn => 11;
+
+        public Production Create(Player owner)
+        {
+            return new TileObjectProduction(this, owner);
+        }
+        public bool IsPlacable(TileObjectProduction production, Terrain.Point point)
+        {
+            return point.Unit == null
+                && point.TileBuilding is CivModel.Common.CityCenter
+                && point.TileBuilding.Owner == production.Owner;
+        }
+        public TileObject CreateTileObject(Player owner, Terrain.Point point)
+        {
+            return new ZapNinja(owner, point);
+        }
+    }
+}
