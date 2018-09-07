728173cf5b01b377afcc9759db089233c1edcb5e
dlarudgus20 <dlarudgus20@naver.com>
2018-09-07 21:05:21 +0900

    #112 implement

--- CivModel.AI/AIController.fs
+++ CivModel.AI/AIController.fs
@@ -1,8 +1,8 @@
 namespace CivModel.AI
 
 open System
-open System.Linq
 open System.Threading.Tasks
+open System.Diagnostics
 open CivModel
 
 type AIController(player: Player) =
@@ -23,16 +23,24 @@ type AIController(player: Player) =
     interface CivModel.IAIController with
         member this.DoAction() =
             async {
-                let context = AIContext player
-                context.QuestComplete <- questComplete
+                try
+                    let context = AIContext player
+                    context.QuestComplete <- questComplete
 
-                let rec action ctx =
-                    let act = getAction ctx
-                    match act with
-                    | Some fn ->
-                        fn ()
-                        action ctx
-                    | None -> ()
-                action context
+                    let rec action ctx count =
+                        if count > 10000 then
+                            Debug.WriteLine "AI infinite loop is detected"
+                        else
+                            let act = getAction ctx
+                            match act with
+                            | Some fn ->
+                                fn ()
+                                action ctx (count + 1)
+                            | None -> ()
+                    action context 0
+                with
+                | ex ->
+                    Debug.WriteLine ("AI uncaught exception: " + (ex.ToString ()))
+                    questComplete.Clear ()
             } |> Async.StartAsTask :> Task
         member this.Destroy() = ()
